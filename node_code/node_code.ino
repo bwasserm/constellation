c811ad9c161f902a72ab263f9e61433e3b7c7fc0
bwasserman <benjamin.wasserman@spacex.com>
2017-05-18 23:06:22 -0700

    Fixed syntax errors in node code, added boom byte

--- node_code/node_code.ino
+++ node_code/node_code.ino
@@ -1,29 +1,31 @@
 #include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h>
 
-USING_RF = 1
-USING_WIFI = (!(USING_RF))
+#define USING_RF 1
+#define USING_WIFI (!(USING_RF))
 
 #define RF_BAUD_RATE 115200
-#define MAX_NUM_NODES 200
+#define MAX_NUM_NODES 100
 #define HEADER "CONSTEL"
 #define HEADER_SIZE strlen(HEADER)
 #define FOOTER "LATION"
 #define FOOTER_SIZE strlen(FOOTER)
 // Packet = CONSTELLATION\0[pixels:rgb]
-#define PACKET_LEN ((HEADER_SIZE) + 3 * (MAX_NUM_NODES) + FOOTER_SIZE)
-#define BUFFER_SIZE (10 * (PACKET_LEN))
+#define PACKET_LEN ((HEADER_SIZE) + 4 * (MAX_NUM_NODES) + FOOTER_SIZE)
+#define BUFFER_SIZE (2 * (PACKET_LEN))
 #define RED_OFFSET 0
 #define GREEN_OFFSET 1
 #define BLUE_OFFSET 2
+#define IGNITER_OFFSET 4
+#define LED_PIN 3
 
-# UPDATE ME FOR EACH NODE
+// UPDATE ME FOR EACH NODE
 #define NODE_INDEX 0
-#define PIXEL_OFFSET ((HEADER_SIZE) + 3 * (NODE_INDEX))
+#define PIXEL_OFFSET ((HEADER_SIZE) + 4 * (NODE_INDEX))
 
 SoftwareSerial radio(2, 3); // RX, TX
 // Num pixels, pin, pixel type
-Adafruit_NeoPixel led = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel led = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 char buffer[BUFFER_SIZE];
 int buf_index = 0;
@@ -52,14 +54,15 @@ void loop() {
   // Find the packet in the buffer
   char *pkt = strstr(buffer, HEADER);
   char *footer = strstr(buffer, FOOTER);
-  int payload_len = int(footer) - int(pkt)
+  int payload_len = int(footer) - int(pkt);
   // If a valid packet was received
-  if(pkt != NULL && footer != NULL && payload_len == (PACKET_LEN - FOOTER_SIZE){
+  if(pkt != NULL && footer != NULL && payload_len == (PACKET_LEN - FOOTER_SIZE)){
     char red = pkt[PIXEL_OFFSET + RED_OFFSET];
     char green = pkt[PIXEL_OFFSET + GREEN_OFFSET];
     char blue = pkt[PIXEL_OFFSET + BLUE_OFFSET];
     // Set LED
     led.setPixelColor(0, red, green, blue);
+    led.show();
     buf_index = 0;
   }
 }
