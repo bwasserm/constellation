23ebed6453c37dab17a9778889c65084ec768a33
bwasserman <benjamin.wasserman@spacex.com>
2017-06-24 23:41:00 -0700

    Added smooth fading rainbow chase

---
+++ ground/rainbow_chase.py
@@ -0,0 +1,32 @@
+import time
+
+import numpy as np
+
+from ground_base import GroundBase
+
+def color(degrees):
+    r = 255 * ((np.sin(degrees / 180. * np.pi) + 1) / 2)
+    g = 255 * ((np.sin((degrees + 120) / 180. * np.pi) + 1) / 2)
+    b = 255 * ((np.sin((degrees + 240) / 180. * np.pi) + 1) / 2)
+    a = 255
+    return r, g, b, a
+
+def main():
+    ground = GroundBase()
+    ground.parse_args()
+    nodes = dict()
+    degrees = 0
+
+    frame_delay = 0.01
+    while True:
+
+        degrees += 1
+
+        # Map each node to a closes
+        for node_id in [1, 3, 2, 4]:
+            nodes[node_id] = color(degrees + node_id * 15)
+            ground.set_nodes(nodes)
+            time.sleep(frame_delay)
+
+if __name__ == '__main__':
+    main()
